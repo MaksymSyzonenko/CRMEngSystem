aed352c421e314290fbf20d35a0f91869361a317
hawaktor@gmail.com <maksymsyzonenkocorporate@gmail.com>
2024-08-04 16:20:40 +0300

    Production

--- Controllers/Control/ControlManualController.cs
+++ Controllers/Control/ControlManualController.cs
@@ -6,13 +6,6 @@ namespace CRMEngSystem.Controllers.Control
     public class ControlManualController : Controller
     {
         [HttpGet]
-        public IActionResult ControlManual()
-        {
-            var fileBytes = Convert.FromBase64String(ConfigurationSettings.Manual);
-            Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
-            Response.Headers.Add("Pragma", "no-cache");
-            Response.Headers.Add("Expires", "0");
-            return File(fileBytes, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "Інструкція.docx");
-        }
+        public IActionResult ControlManual() => File(Convert.FromBase64String(ConfigurationSettings.Manual), "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "Інструкція.docx");
     }
 }
