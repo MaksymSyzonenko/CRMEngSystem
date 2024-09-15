af63c2730cb9d97af6c57f50db2ae91563a06e44
hawaktor@gmail.com <maksymsyzonenkocorporate@gmail.com>
2024-09-15 12:50:36 +0300

    Update GarbageCollectorService.cs

--- Services/Garbage/GarbageCollectorService.cs
+++ Services/Garbage/GarbageCollectorService.cs
@@ -16,21 +16,20 @@
             while (!stoppingToken.IsCancellationRequested)
             {
                 var now = DateTime.Now;
-                var nextRunTime = now.Date.Add(new TimeSpan(12, 45, 0)); // Каждый день в 20:00
+                var nextRunTime = now.Date.Add(new TimeSpan(12, 45, 0));
                 if (nextRunTime < now)
                 {
-                    nextRunTime = nextRunTime.AddDays(1); // Если текущее время после 20:00, то следующая дата - завтра
+                    nextRunTime = nextRunTime.AddDays(1);
                 }
 
-                var delay = nextRunTime - now; // Рассчитываем время до следующего запуска
+                var delay = nextRunTime - now;
                 _logger.LogInformation($"Следующая очистка памяти запланирована на {nextRunTime}");
 
-                await Task.Delay(delay, stoppingToken); // Ожидаем до 20:00
+                await Task.Delay(delay, stoppingToken);
 
-                // Запуск сборки мусора
                 _logger.LogInformation("Запуск сборки мусора.");
                 GC.Collect();
-                GC.WaitForPendingFinalizers(); // Ожидаем завершения финализаторов
+                GC.WaitForPendingFinalizers();
 
                 _logger.LogInformation("Сборка мусора завершена.");
             }
